5e9f4d2485ddca294f6c294e900acafba679d665
PredaMarius <predamarius2007@yahoo.com>
2019-10-31 10:34:41 +0200

    github pages

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/PredaMarius/PredaMarius.gitHub.io-larexirWeb/precache-manifest.8110004140573d23656a7fc918bd092a.js"
+  "/larexirWeb/precache-manifest.ffc64d2e616d3b539f5be1a1061970e5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PredaMarius/PredaMarius.gitHub.io-larexirWeb/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/larexirWeb/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
