fed9a9f0fb330b6d04d1f469f5c54bd73f3dafb9
PredaMarius <predamarius2007@yahoo.com>
2019-11-01 08:47:06 +0200

    Updates

---
+++ precache-manifest.029532fe5a4198ba434cc2815e215966.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd521281c66486fb0a481541dd0d7e6",
+    "url": "/larexirWeb/index.html"
+  },
+  {
+    "revision": "d40cccdacd6acaff0b61",
+    "url": "/larexirWeb/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4518a5dda1d2671c23ef",
+    "url": "/larexirWeb/static/css/main.ac51d52b.chunk.css"
+  },
+  {
+    "revision": "d40cccdacd6acaff0b61",
+    "url": "/larexirWeb/static/js/2.12507d21.chunk.js"
+  },
+  {
+    "revision": "4518a5dda1d2671c23ef",
+    "url": "/larexirWeb/static/js/main.06e1a5ec.chunk.js"
+  },
+  {
+    "revision": "3c81123bae044f5dc9fb",
+    "url": "/larexirWeb/static/js/runtime~main.2013e556.js"
+  },
+  {
+    "revision": "c26ff6c4075b002f01e9706ccbb3b3a8",
+    "url": "/larexirWeb/static/media/shopping-bag.c26ff6c4.svg"
+  }
+]);
